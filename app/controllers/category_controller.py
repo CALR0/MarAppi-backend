0e917ca7e06995578b0e5f3f1ced33ac66411896
Carlos Lizarazo <carlosandr3zlizarazoromero@gmail.com>
2025-12-06 18:32:55 -0500

    docs: add Flasgger docstrings to category endpoints

--- app/controllers/category_controller.py
+++ app/controllers/category_controller.py
@@ -4,20 +4,95 @@ from ..schemas import category_schema, categories_schema
 
 
 def create_category():
-    # Let ValidationError bubble to the global handler
-    data = category_schema.load(request.get_json() or {})
-    category = category_service.create_category(data)
-    return jsonify(category_schema.dump(category)), 201
+        """
+        Create a new category
+        ---
+        tags:
+            - Categories
+        requestBody:
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            name:
+                                type: string
+                        required:
+                            - name
+        responses:
+            201:
+                description: Created
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                id:
+                                    type: integer
+                                name:
+                                    type: string
+        """
+        # Let ValidationError bubble to the global handler
+        data = category_schema.load(request.get_json() or {})
+        category = category_service.create_category(data)
+        return jsonify(category_schema.dump(category)), 201
 
 
 def list_categories():
-    cats = category_service.get_all_categories()
-    return jsonify(categories_schema.dump(cats)), 200
+        """
+        List categories
+        ---
+        tags:
+            - Categories
+        responses:
+            200:
+                description: OK
+                content:
+                    application/json:
+                        schema:
+                            type: array
+                            items:
+                                type: object
+                                properties:
+                                    id:
+                                        type: integer
+                                    name:
+                                        type: string
+        """
+        cats = category_service.get_all_categories()
+        return jsonify(categories_schema.dump(cats)), 200
  
 
 def get_category(category_id):
-    cat = category_service.get_category(category_id)
-    return jsonify(category_schema.dump(cat)), 200
+        """
+        Get a category by id
+        ---
+        tags:
+            - Categories
+        parameters:
+            - in: path
+                name: category_id
+                schema:
+                    type: integer
+                required: true
+                description: Numeric ID of the category to get
+        responses:
+            200:
+                description: OK
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                id:
+                                    type: integer
+                                name:
+                                    type: string
+            404:
+                description: Not Found
+        """
+        cat = category_service.get_category(category_id)
+        return jsonify(category_schema.dump(cat)), 200
 
 
 def update_category(category_id):
